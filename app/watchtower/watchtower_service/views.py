0aef22ddfb0934beb7ea7851eb9058d1e80db47d
Nick Cleary <nicholascleary01@gmail.com>
2022-07-29 23:34:35 -0400

    Updates

--- app/watchtower/watchtower_service/views.py
+++ app/watchtower/watchtower_service/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+import json
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from watchtower_service.models import WebhookEvent
 
 
 class ListView(APIView):
     def get(self, request, format=None):
         return Response({"ping": "pong2"})
 
+
 class WebHookEventView(APIView):
     def post(self, request, format=None):
-        json = request.data
-        print(json)
+        contents = request.data
+        WebhookEvent(event_json=contents).save()
         return Response({"ping", "Pong2"})
